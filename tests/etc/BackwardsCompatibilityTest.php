fbcf60501d36652cfb8fe559c560ed30f4ba2af7
mnito <mpnitowski@gmail.com>
2016-09-21 23:17:25 -0400

    Add backwards compatibility test for schedule function

---
+++ tests/etc/BackwardsCompatibilityTest.php
@@ -0,0 +1,13 @@
+<?php
+
+use PHPUnit\Framework\TestCase;
+
+class BackwardsCompatibilityTest extends TestCase
+{
+    public function testScheduleBackwardsCompatibility()
+    {
+        $teams = ['Team 1', 'Team 2', 'Team 3', 'Team 4', 'Team 5'];
+        $this->assertEquals(schedule($teams, 17, false), make_schedule($teams, 17, false));
+        $this->assertEquals(schedule($teams, null, true, 16), make_schedule($teams, null, true, 16));
+    }
+}
